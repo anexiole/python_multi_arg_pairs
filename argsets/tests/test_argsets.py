c5e3d6841279454d3c82c23b94ea996d93812e26
Gordon Yeong <gordon.yeong@broadsource.com.au>
2021-06-03 15:42:25 +1000

    basic test working

--- argsets/tests/test_argsets.py
+++ argsets/tests/test_argsets.py
@@ -1,5 +1,9 @@
 from argsets import __version__
-
+from argsets.argsets import hello  # this means, in argsets/argsets.py, import the function, hello
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def  test_world():
+    assert hello() == "world"
