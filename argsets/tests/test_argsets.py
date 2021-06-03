0ffc2af1409f0b2bda554ec5cef324502cb217ab
Gordon Yeong <gordon.yeong@broadsource.com.au>
2021-06-03 16:54:49 +1000

    this is erroring on test run because it seems we can't force Argparse  to do what we want to do . All good

--- argsets/tests/test_argsets.py
+++ argsets/tests/test_argsets.py
@@ -1,9 +1,33 @@
 from argsets import __version__
-from argsets.argsets import hello  # this means, in argsets/argsets.py, import the function, hello
+from argsets.argsets import hello, ParseKwargs # this means, in argsets/argsets.py, import the function, hello and the class, ParseKwargs
 import pytest
+import argparse
 
 def test_version():
     assert __version__ == '0.1.0'
 
 def  test_world():
     assert hello() == "world"
+
+
+def test_pairs():
+    # creating parser pbject
+    parser = argparse.ArgumentParser()
+      
+    # adding an arguments 
+#    parser.add_argument('--kwargs', 
+#                        nargs='*', 
+#                        action = ParseKwargs)
+    parser.add_argument('--server', 
+                        nargs='*', 
+                        action = ParseKwargs)
+
+    raw_args = [ '--server', 'One', '--server' , 'Two', '--server', 'Three' ]
+      
+     #parsing arguments 
+    args = parser.parse_args(raw_args)
+      
+    # show the dictionary
+    print(args.kwargs)
+
+    assert 1 == 1
